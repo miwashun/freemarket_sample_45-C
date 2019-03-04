4ce4964c4afafaa351d3c909919f251e37a31816
miwashun <miwashun@miwanomadokanoMacBook-Pro.local>
2019-03-04 15:41:24 +0900

    renovate
    WHY
    データが古いので更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   get 'items/index'
   get 'items/show'
   devise_for :users
   root 'items#index'
+  resources :users, only: :index
   resources :profiles, only: :index
 
 end
